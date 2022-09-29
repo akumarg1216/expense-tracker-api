82f47a925d8b613096ca7cf124e1900bc0cd8bda
ANKIT KUMAR GUPTA <akumarg1216@gmail.com>
2022-09-29 16:49:24 +0530

    Added the first test & github actions

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from unittest import TestCase
 
 # Create your tests here.
+def two_integers_sum(a, b):
+    return a + b
+
+
+class TestSun(TestCase):
+    def test_sum(self):
+        self.assertEqual(two_integers_sum(1, 2), 3)
